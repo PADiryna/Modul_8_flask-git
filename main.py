da72a3fa5bd76d17f703f65bac42a42e72309deb
Iryna Padafet <irini-84@ukr.net>
2023-02-03 21:05:27 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+@app.route('/page1', methods=['GET'])
+def greeting():
+    return render_template('greeting.html')
+
+@app.route('/page2', methods=['GET', "POST"])
+def contact():
+   if request.method == 'GET':
+       return render_template('contact.html')
+   elif request.method == 'POST':
+       print(request.form)
+
+
+if __name__ ==  '__main__':
+    app.run()       
