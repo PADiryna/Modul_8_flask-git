3bb46cffef7e1073f2478f2262b153a65efc45d9
Iryna Padafet <irini-84@ukr.net>
2023-02-10 23:54:16 +0100

    Add main.py

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 from flask import Flask, render_template, url_for, request, redirect
 
-
 app = Flask(__name__)
 
+menu = [{'name':'About me', 'url':'my page'},
+        {'name':'Contact', 'url':'contact'},
+        {'name':'Send a message', 'url':'feedback'}]
 
-@app.route('/', methods=['GET'])
-@app.route('/page1', methods=['GET'])
+@app.route('/favicon.ico')
+def favicon():
+    return url_for('static', filename='images/favicon.ico')        
+
+
+@app.route('/')
 def greeting():
-    return render_template('greeting.html')
+    return render_template('greetings.html', menu = menu)
 
-@app.route('/page2', methods=['GET', "POST"])
+@app.route('/contact')
 def contact():
-   if request.method == 'GET':
-       return render_template('contact.html')
-   elif request.method == 'POST':
-       print(request.form)
-       return redirect('/page2')
+    return render_template('contact.html', menu = menu)
+
+
+@app.route('/feedback', methods=['GET', "POST"])   
+def feedback():
+    if request.method == 'POST':
+         print(request.form)
+    return render_template('feedback.html', menu = menu)  
 
 
 if __name__ ==  '__main__':
